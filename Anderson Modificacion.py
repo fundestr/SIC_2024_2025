8e9ab5f0914d630db857ed7c3144d246757e73e7
AndersonP444 <andersonjperdomo@gmail.com>
2024-11-16 10:08:36 -0400

    Add files via upload

---
+++ Anderson Modificacion.py
@@ -0,0 +1,8 @@
+try:
+	name = input('Ingrese nombre: ')
+	print(f'Hola {name}!')
+	print('\nEdad: ', int(input('Ingrese edad: ')))
+except:
+	print('Dato incorrecto!')
+
+print("Soy Anderson y tengo 21 :3")
